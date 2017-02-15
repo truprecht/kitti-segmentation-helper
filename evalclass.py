473bd981682f73adf5fd1ee81b6508f5045dea58
Thomas Ruprecht <thomas.ruprecht@tu-dresden.de>
2017-02-15 13:43:10 +0100

    *

--- evalclass.py
+++ evalclass.py
@@ -1,7 +1,4 @@
 import numpy as np
-from h5py import File
-#from scipy.io import loadmat
-
 import cv2
 from sys import argv
 
@@ -11,16 +8,18 @@ gt = cv2.imread(argv[1], cv2.IMREAD_GRAYSCALE)
 gtheight, gtwidth = gt.shape
 
 try:
+    from scipy.io import loadmat
     # (41,      41,     6)
     # (height,  width,  channels)
-    #pred = loadmat(argv[2])["data"][:, :, :, 0]
-    raise Exception("")
+    pred = loadmat(argv[2])["data"][:, :, :, 0]
 except Exception, e:
+    from h5py import File
     # sometimes caffe decides to return matlab 7.3 matrices...
     # (6,           41,     41)
     # (channels,    width,  height)
     pred = File(argv[2])["data"][0]
     pred = np.swapaxes(pred, 0, 2)
+    pred = np.swapaxes(pred, 0, 1)
 
 pred = cv2.resize(pred, (gtwidth, gtheight))
 classpred = np.array(np.argmax(pred, axis=2) != 0, dtype=np.uint8)
@@ -30,8 +29,9 @@ fp = np.logical_and(classpred, np.logical_not(gt))
 fn = np.logical_and(np.logical_not(classpred), gt)
 
 tp_, fp_, fn_ = np.sum(tp), np.sum(fp), np.sum(fn)
+print tp_, fp_, fn_
 
 precision = float(tp_) / (fp_ + tp_)
 recall = float(tp_) / (fn_ + tp_)
 
-print "%d, %d" %(precision, recall)
+print "%f, %f" %(precision, recall)
