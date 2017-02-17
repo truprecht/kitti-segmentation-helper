380f7dc883a1493db2738c6c77c9befa5135eaae
Thomas Ruprecht <thomas.ruprecht@tu-dresden.de>
2017-02-17 12:33:39 +0100

    *

---
+++ eval-scripts/run_cnn.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+#SBATCH --time=2:00:00
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=3
+#SBATCH --mem-per-cpu=3000M
+#SBATCH --mail-user=thomas.ruprecht@tu-dresden.de
+#SBATCH --mail-type=END,FAIL
+
+
+function path() {
+    if [ "${1: -1}" != "/" ]
+    then
+        echo $1/
+    else
+        echo $1
+    fi
+}
+
+if [ -z $1 ] || [ -z $2 ] || [ -z $3 ] || [ -z $4 ]
+then
+    echo "use $0 <project root> <image list> <scripts> <output folder>"
+    exit 1
+fi
+
+ROOT=$(path $1)
+IMAGELIST=$2
+SCRIPTS=$(path $3)
+OUT=$(path $4)
+
+PROTOTXT="${ROOT}cnn/test.prototxt"
+CAFFEMOD="${ROOT}cnn/cityscapes.caffemodel"
+
+DATA="temp/"
+CNNOUT="fc8_val3769/"
+PATCHLIST="test_list.txt"
+PATCHLISTID="test_list_id_only.txt"
+
+CRFINPUT="${OUT}input/"
+CRFROI="${OUT}roi/"
+CRFIMAGE="${OUT}image/"
+
+SWIDTH=275
+SHEIGHT=330
+SSTRIDE=50
+
+MWIDTH=400
+MHEIGHT=500
+MSTRIDE=80
+
+LWIDTH=600
+LHEIGHT=750
+LSTRIDE=120
+
+#
+# cut image patches
+# call patches.py <image> <patchwidth> <patchheight> <stride in x direction> <offset in y direction> <output folder> <output name postfix>
+# st. output name := basename + postfix + autoincrement + file postfix, 
+# if input name = basename + file postfix
+#
+rm -r $DATA &> /dev/null
+mkdir -p $DATA
+
+mkdir -p $CRFINPUT
+mkdir -p $CNNOUT
+
+for IMAGE in $(cat $IMAGELIST)
+do
+    rm ${PATCHLIST}
+    touch ${PATCHLIST}
+    if [ $IMAGE == "*_small.png" ]
+    then
+        python2 ${SCRIPTS}patchesv2.py $IMAGE $(($SWIDTH/4)) $(($SHEIGHT/4)) $(($SSTRIDE/4)) $(($LHEIGHT/4)) ${DATA} "_3" >> ${PATCHLIST}
+        python2 ${SCRIPTS}patchesv2.py $IMAGE $(($MWIDTH/4)) $(($SHEIGHT/4)) $(($MSTRIDE/4)) $(($LHEIGHT/4)) ${DATA} "_2" >> ${PATCHLIST}
+        python2 ${SCRIPTS}patchesv2.py $IMAGE $(($LWIDTH/4)) $(($LHEIGHT/4)) $(($LSTRIDE/4)) $(($LHEIGHT/4)) ${DATA} "_1" >> ${PATCHLIST}
+    elif [ $IMAGE == "*_sparse.png" ]
+    then
+        python2 ${SCRIPTS}patchesv2.py $IMAGE $SWIDTH $SHEIGHT $(($SSTRIDE*2)) $LHEIGHT ${DATA} "_3" >> ${PATCHLIST}
+        python2 ${SCRIPTS}patchesv2.py $IMAGE $MWIDTH $MHEIGHT $(($MSTRIDE*2)) $LHEIGHT ${DATA} "_2" >> ${PATCHLIST}
+        python2 ${SCRIPTS}patchesv2.py $IMAGE $LWIDTH $LHEIGHT $(($LSTRIDE*2)) $LHEIGHT ${DATA} "_1" >> ${PATCHLIST}
+    else
+        python2 ${SCRIPTS}patchesv2.py $IMAGE $SWIDTH $SHEIGHT $SSTRIDE $LHEIGHT ${DATA} "_3" >> ${PATCHLIST}
+        python2 ${SCRIPTS}patchesv2.py $IMAGE $MWIDTH $MHEIGHT $MSTRIDE $LHEIGHT ${DATA} "_2" >> ${PATCHLIST}
+        python2 ${SCRIPTS}patchesv2.py $IMAGE $LWIDTH $LHEIGHT $LSTRIDE $LHEIGHT ${DATA} "_1" >> ${PATCHLIST}
+    fi
+    
+    python2 ${SCRIPTS}idonly.py $PATCHLIST > $PATCHLISTID
+    iterations=$(wc -w $PATCHLISTID  | grep -o "^[0-9]\+")
+    srun caffe test -model=$PROTOTXT -weights=$CAFFEMOD -iterations $iterations -gpu 0
+    mv $CNNOUT/*_3_* ${CRFINPUT}${IMAGE}/small
+    mv $CNNOUT/*_2_* ${CRFINPUT}${IMAGE}/medium
+    mv $CNNOUT/*_1_* ${CRFINPUT}${IMAGE}/large
+done
+
+
+# move roi files to densecrf
+mkdir -p $CRFROI
+mv ${DATA}*.txt $CRFROI
+mv ${DATA}*.txt $CRFROI
+mv ${DATA}*.txt $CRFROI
+rm -r ${DATA}
+
+# move image to densecrf
+mkdir -p $CRFIMAGE
+for IMAGE in $(cat $IMAGELIST)
+do
+    cp $IMAGE $CRFIMAGE
+done
+
+tar -czf ${OUT}input.tar.gz $CRFINPUT
+rm -r $CRFINPUT
+tar -czf ${OUT}image.tar.gz $CRFIMAGE
+rm -r $CRFIMAGE
+tar -czf ${OUT}roi.tar.gz $CRFROI
+rm -r $CRFROI
---
+++ eval-scripts/test_densecrf.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=10583M
+#SBATCH --mail-user=thomas.ruprecht@tu-dresden.de
+#SBATCH --mail-type=END,FAIL
+
+export OMP_NUM_THREADS=12
+
+
+if [ -z $1 ]
+then
+    echo "use $0 <input folder> <scripts folder> [<output folder>]"
+    exit 1
+fi
+
+function path() {
+    if [ "${1: -1}" != "/" ]
+    then
+        echo $1/
+    else
+        echo $1
+    fi
+}
+ROOT=$(path $1)
+IMAGES=${ROOT}image.tar.gz
+PATCHES=${ROOT}input.tar.gz
+ROIS=${ROOT}roi.tar.gz
+
+tar -xzf $IMAGES
+tar -xzf $PATCHES
+tar -xzf $ROIS
+
+PATCHES=${ROOT}input
+IMAGES=${ROOT}image
+ROIS=${ROOT}roi
+LIST=${PATCHES}/filelist.txt
+
+SCRIPTS=$(path $2)
+
+if [ -z $4 ]
+then
+    OUT="CRFRESULT/"
+else
+    OUT=$(path $4)
+fi
+
+
+# run inference
+mkdir -p $OUT
+
+
+# resample classifications to original patch size, move w/ to densecrf
+# call resample.py <input dir> <resampled width> <resampled height> <output dir>
+# only resamples *.mat files, 
+# output name := basename + .dat, if input name = basename + _blob_0.mat
+for patchset in $PATCHES/*
+do
+    SWIDTH=275
+    SHEIGHT=330
+
+    MWIDTH=400
+    MHEIGHT=500
+
+    LWIDTH=600
+    LHEIGHT=750
+
+    touch $LIST
+
+    if [ $patchset == "*_small.png" ]
+    then
+        python2 ${SCRIPTS}resample.py $patchset/small $(($SWIDTH/4)) $(($SHEIGHT/4)) $PATCHES
+        python2 ${SCRIPTS}resample.py $patchset/medium $(($MWIDTH/4)) $(($MHEIGHT/4)) $PATCHES
+        python2 ${SCRIPTS}resample.py $patchset/large $(($LWIDTH/4)) $(($LHEIGHT/4)) $PATCHES
+    else
+        python2 ${SCRIPTS}resample.py $patchset/small $SWIDTH $SHEIGHT $PATCHES
+        python2 ${SCRIPTS}resample.py $patchset/medium $MWIDTH $MHEIGHT $PATCHES
+        python2 ${SCRIPTS}resample.py $patchset/large $LWIDTH $LHEIGHT $PATCHES
+    fi
+    wl=1 # weight for local CNN prediction term (large patches)
+    wm=1.7 # weight for local CNN prediction term (medium patches)
+    ws=1.7 # weight for local CNN prediction term (small patches)
+    sp=0.1 # stddev in the kernel
+
+    wi=12 # weight for inter-connected component term
+    df=0.6 # threshold for obtaining foreground map
+
+    wlocc=1.7 # weight for smoothness term
+    slocl=80 # spatial stddev
+    slocpr=0.2 # CNN prediction stddev
+    iters=50 # iterations of mean field to run
+
+    echo "begin inference of $IMAGEFILTER $(date)" > timelog.txt 
+    srun inference -p $LIST -ws ${ws} -wm ${wm} -wl ${wl} -wi ${wi} -sp ${sp} -df ${df} -wc ${wc} -wp ${wp} -sps ${sps} -wcol ${wcol} -wlocc ${wlocc} -wlocp ${wlocp} -slocl ${slocl} -slocpr ${slocpr} -iters ${iters} -o $OUT
+    echo "end inference of $IMAGEFILTER $(date)" > timelog.txt 
+    
+    rm $PATCHES/*.dat $LIST
+done
